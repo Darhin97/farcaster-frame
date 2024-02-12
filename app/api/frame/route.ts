7674276ec6d237c947d2e2d95d0841db9172dbf9
Derrick Ankrah <darhin97@gmail.com>
2024-02-12 22:21:47 +0000

    fix image url

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -12,7 +12,7 @@ async function getResponse(req: NextRequest):Promise<NextResponse>  {
         return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame 5</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qma9ebPKfMaSqDdqeioE81dvm261LRrwWfjVuUhJAFBSXw/5.png" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmXKheKKQjeJLDiieJc83gMcrCk6m58FjqpQeeiNDsojCh/5.jpg" />
     <meta property="fc:frame:image:aspect_ratio" content="1:1" />
     <meta property="fc:frame:button:1" content="Visit Naruto and Boruto FanClub" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
@@ -24,7 +24,7 @@ async function getResponse(req: NextRequest):Promise<NextResponse>  {
         return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qma9ebPKfMaSqDdqeioE81dvm261LRrwWfjVuUhJAFBSXw/${id}.png" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmXKheKKQjeJLDiieJc83gMcrCk6m58FjqpQeeiNDsojCh/${id}.jpg" />
    <meta property="fc:frame:image:aspect_ratio" content="1:1" />
     <meta property="fc:frame:button:1" content="Next Page" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import {Metadata} from "next";
 
 const frameMetadata = getFrameMetadata({
   buttons: [{label: "Begin"}],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qma9ebPKfMaSqDdqeioE81dvm261LRrwWfjVuUhJAFBSXw/0.png`,
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmXKheKKQjeJLDiieJc83gMcrCk6m58FjqpQeeiNDsojCh/0.jpg`,
   postUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 
 
@@ -17,7 +17,7 @@ export const metadata:Metadata ={
   openGraph: {
     title: 'Anime FanClub',
     description: 'A fun club for boruto and naruto lovers',
-    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qma9ebPKfMaSqDdqeioE81dvm261LRrwWfjVuUhJAFBSXw/0.png`]
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmXKheKKQjeJLDiieJc83gMcrCk6m58FjqpQeeiNDsojCh/0.jpg`]
   },
   other: {
     ...frameMetadata
